0d9b6f0a9146bd8a34a817290c8248bed2a9fc82
RaffayIrfan <78643689+RaffayIrfan@users.noreply.github.com>
2025-09-23 09:34:47 +0500

    Add files via upload
    Added Raffay irfan’s folder with tasks

---
+++ Raffay irfan/Introduction.py
@@ -0,0 +1,4 @@
+Name = input("Enter your name : ")
+Age = int(input("Enter your age : "))
+Height = float(input("Enter your height in ft : "))
+print(Name, "is",Age, "years old and is",Height, "Ft tall")
