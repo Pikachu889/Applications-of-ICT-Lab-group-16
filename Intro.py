d02551ee3034bfed7caea47ce19c22aacac34907
Pikachu889 <loginf301@gmail.com>
2025-09-23 09:31:41 +0500

    Delete Intro.py

--- Intro.py
+++
@@ -1,4 +0,0 @@
-Name = input("Enter your name : ")
-Age = int(input("Enter your age : "))
-Height = float(input("Enter your height in ft : "))
-print(Name, "is",Age, "years old and is",Height, "Ft tall")
